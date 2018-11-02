a042503bf15199f6723f9029aef04b36d4c81706
Unknown <ar.birdsall.jobs@gmail.com>
2018-11-02 16:58:43 +0000

    Add 'vehiclelist' to routes

--- ContactApp/src/app/app.routing.ts
+++ ContactApp/src/app/app.routing.ts
@@ -2,12 +2,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ContactlistComponent } from './contactlist/contactlist.component';
-import { ContactformComponent } from './contactform/contactform.component';
+import { ContactlistComponent } from '@components/contactlist/contactlist.component';
+import { ContactformComponent } from '@components/contactform/contactform.component';
+import { VehiclelistComponent } from '@components/vehiclelist/vehiclelist.component';
 
 const appRoutes: Routes = [
   { path: '',  pathMatch: 'full' , component: ContactlistComponent },
-  { path: 'contactform', component: ContactformComponent }
+  { path: 'contactform', component: ContactformComponent },
+  { path: 'vehiclelist', component: VehiclelistComponent }
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
