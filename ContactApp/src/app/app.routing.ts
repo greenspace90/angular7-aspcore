c6e6314ca2e15e7d4c51bbd54a2d044a6a09e529
Unknown <ar.birdsall.jobs@gmail.com>
2018-11-08 09:46:02 +0000

    Add vehicleform to routing

--- ContactApp/src/app/app.routing.ts
+++ ContactApp/src/app/app.routing.ts
@@ -5,12 +5,22 @@ import { AppComponent } from './app.component';
 import { ContactlistComponent } from '@components/contactlist/contactlist.component';
 import { ContactformComponent } from '@components/contactform/contactform.component';
 import { VehiclelistComponent } from '@components/vehiclelist/vehiclelist.component';
+import { VehicleformComponent } from '@components/vehicleform/vehicleform.component';
 
 const appRoutes: Routes = [
   { path: '',  pathMatch: 'full' , component: ContactlistComponent },
   { path: 'contactform', component: ContactformComponent },
-  { path: 'vehiclelist', component: VehiclelistComponent }
+  { path: 'vehiclelist', component: VehiclelistComponent },
+  { path: 'vehicleform', component: VehicleformComponent }
 ];
+
+// https://stackoverflow.com/questions/46771315/angular-cannot-get
+// const appRoutes: Routes = [
+// { path: '', component: ContactlistComponent, children: [ 
+// { path: '', redirectTo: 'contact', pathMatch: 'full' },   
+// { path: 'contactform', component: ContactformComponent },
+// { path: 'vehiclelist', component: VehiclelistComponent },
+// { path: 'vehicleform', component: VehicleformComponent }]}];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
