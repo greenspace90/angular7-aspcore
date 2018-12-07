65480daf046cba93b35e3391d6ed3c9f01b9caf3
Unknown <ar.birdsall.jobs@gmail.com>
2018-12-07 13:58:51 +0000

    Settings model

---
+++ ContactApp/src/app/_models/settings.ts
@@ -0,0 +1,9 @@
+export interface ISettings {
+    settingsId: number,
+    chartTitleFontSize: number;
+    chartScaleLabelFontSize: number;
+    chartLineWidth: number;
+    chartLineColour: string;
+    chartAreaBackgroundColour: string;
+    chartModalBackgroundColour: string;
+}
